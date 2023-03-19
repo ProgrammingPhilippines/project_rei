c89d2f8ffa5a42d268b9e5e71beee1973fc45a5f
Johnny <hddnblde@gmail.com>
2023-03-19 11:35:44 +0800

    Implemented input based on joystick.

---
+++ Assets/[ProjectRei]/Scripts/Runtime/Input/IsometricJoystickInput.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace GameInput
+{
+    public sealed class IsometricJoystickInput : IsometricPlayerInput
+    {
+        #region Fields
+        [SerializeField]
+        private StickInput m_movementInput = StickInput.Standard;
+        
+        [SerializeField]
+        private StickInput m_headingInput = StickInput.Standard;
+        #endregion
+
+
+        #region IsometricPlayerInput Implementation
+        protected override Vector2 faceDirection =>
+            TransformRelativeToCameraDirection(m_headingInput.direction);
+
+        protected override Vector2 movementDirection =>
+            m_movementInput.direction;
+        #endregion
+
+
+        #region Method
+        private Vector2 TransformRelativeToCameraDirection(Vector2 stickInput)
+        {
+            if (cameraComponent == null)
+                return stickInput;
+
+            Quaternion cameraYawRotation = GetCameraYawRotation();
+
+            Vector3 transformedInput = cameraYawRotation *
+                StickControlToWorldDirection(stickInput);
+
+            return new Vector2(transformedInput.x, -transformedInput.z);
+        }
+        #endregion
+    }
+}
